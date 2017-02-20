6b164f02a32e16d4a83c6b18af6da6a8581fa769
3axap4eHko <3axap4eHko@gmail.com>
2017-02-19 23:43:17 -0500

    modules upgrade

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -2,7 +2,7 @@ const Path = require('path');
 const {DefinePlugin, optimize} = require('webpack');
 const BabiliPlugin = require('babili-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ExtractPostCss = new ExtractTextPlugin('/css/[name].css');
+const ExtractPostCss = new ExtractTextPlugin('css/[name].css');
 const Html = require('html-webpack-plugin');
 const Copy = require('copy-webpack-plugin');
 const WebpackPlugin = require('./webpack.plugin');
@@ -15,13 +15,13 @@ module.exports = {
   },
   output: {
     path: Path.resolve(__dirname, '../build'),
-    filename: '/js/[name].js',
-    chunkFilename: '/js/[id].js'
+    filename: 'js/[name].js',
+    chunkFilename: 'js/[id].js'
   },
   module: {
     rules: [
       {test: /\.jsx?$/, exclude: /node_modules/, loader: 'eslint-loader', enforce: 'pre'},
-      {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', options: {cacheDirectory: 'cache'}},
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
       {test: /\.css$/, loader: ExtractPostCss.extract(['css-loader', 'postcss-loader'])},
       {test: /\.(svg|jpg|png|gif)$/, loader: 'file-loader', options: {name: 'images/[name].[ext]'}},
       {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader', options: {name: 'fonts/[name].[ext]', limit: 5000, mimetype: 'application/font-woff'}},
